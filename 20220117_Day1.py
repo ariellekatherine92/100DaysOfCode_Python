b5b334664626786a3cf4b785216e12d963619660
Ariel Steger <ariellekatherine92@gmail.com>
2023-01-18 11:46:09 -0500

    day 1 project

---
+++ 20220117_Day1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?")
+name = input("What is the name of your pet?")
+print("Your band name is")
+print(city + name)
